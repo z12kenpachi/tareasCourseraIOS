9a6ade4e6b3b470810fab5b9338b1adb37e8a4de
z12kenpachi <danielxd.rodriguez3@gmail.com>
2017-01-02 17:10:46 -0600

    Add files via upload

---
+++ TareaRangoDanielRodriguez.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+// Tarea segunda semana
+// 2 Enero del 2017
+// Daniel Rodriguez
+
+for numero in 0...100 {
+    
+    if numero % 5 == 0 {
+    
+        print("\(numero) Bingo!!!")
+        
+    }
+    
+    if numero % 2 == 0 {
+    
+        print("\(numero) par!!!")
+        
+    }
+    
+    if numero % 2 != 0 {
+    
+        print("\(numero) impar!!!")
+        
+    }
+    
+    if numero >= 30 && numero <= 40 {
+    
+        print("\(numero) Viva Swift!!!")
+        
+    }
+    
+}
+
+
